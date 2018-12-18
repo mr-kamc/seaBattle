0b76001ab3cdb8cabf33251819ed4090fd0ede6e
mrkam <mr.kamc@gmail.com>
2018-12-18 14:47:52 +0300

    new SeaBattle

---
+++ src/DotCom.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class DotCom {
+    private ArrayList<String> locationCells;
+    private String name;
+
+
+    public void setLocationCells(ArrayList<String> loc) {
+        locationCells = loc;
+    }
+
+    public void setName(String n) {
+        name = n;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String checkYourSelf(String userInput) {
+        String result = "Mимо";
+        int index = locationCells.indexOf(userInput);
+        if (index >= 0) {
+            locationCells.remove(index);
+            if (locationCells.isEmpty()) {
+                result = "Потопил";
+                System.out.println("Ой! Вы потопили " + name + " :-(");
+            } else {
+                result = "Попал";
+            }
+        }
+        return result;
+    }
+}
